2e4679840b2d292ef76759fa2017d547d309c4b8
madeiramcqueen <madeira@mcqueen.net>
2022-11-07 14:08:29 -0800

    Add new page and route for new subject form with navbar

--- controllers/subjects.js
+++ controllers/subjects.js
@@ -2,10 +2,15 @@ const Subject = require("../models/subject");
 
 module.exports = {
     index,
+    new: newSubject
 };
 
 function index(req, res) {
     Subject.find({}, function (err, subjects){
         res.render("subjects/index", {title: "Class Subjects", subjects})
     });
+}
+
+function newSubject(req, res) {
+    res.render('subjects/new', {title: "New Subject"})
 }
--- routes/subjects.js
+++ routes/subjects.js
@@ -5,4 +5,7 @@ var subjectsCtrl = require("../controllers/subjects");
 //Get the Subjects page
 router.get('/', subjectsCtrl.index);
 
+//Make a new skill
+router.get('/new', subjectsCtrl.new);
+
 module.exports = router;
